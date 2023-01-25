5713cd78766f0a3b64cb44247c8fb3a7e4bc722e
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-01-25 13:10:03 -0500

    add notes

--- src/CompositionContainer.js
+++ src/CompositionContainer.js
@@ -1,8 +1,13 @@
-import CardNav from './CardNav';
+import CardNav from "./CardNav";
 
-const Container = props => {
+//this container is the card that is repeated design wise.
+// It always has a navbar component and then whatever child that is inserted.
+//Notice then CardNav is dynamic and takes a prop to change the title which is passed in the App.js when calling the Container Component
+//check App.js to see how the Container component is used
+
+const Container = (props) => {
   return (
-    <div className='container'>
+    <div className="container">
       <CardNav title={props.title} />
       {props.children}
     </div>
